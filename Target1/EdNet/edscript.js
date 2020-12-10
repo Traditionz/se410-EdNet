cfafe5dd18114acd3834e73741e389d998fdd7f8
Brian Tran <brian@Brians-MBP.fios-router.home>
2020-12-09 22:01:14 -0500

    adding the source code

---
+++ Target1/EdNet/edscript.js
@@ -0,0 +1,19 @@
+function showTab(tabName) {
+  var i, tabcontent;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  document.getElementById(tabName).style.display = "block";
+}
+
+function clickInner(pageName) {
+    window.location.href = pageName;
+}
+
+
+function toggleFilter(dropDown) {
+  document.getElementById(dropDown).classList.toggle("show");
+}
